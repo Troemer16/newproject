55bf0b94fce5a6e46e6e57280f15970d2f65a3dc
Tyler Roemer <troemer@mail.greenriver.edu>
2018-01-11 14:17:38 -0500

    made project fat-free

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    //require the autoload file
+    require_once ('vendor/autoload.php');
+
+    //create an instance of the base class
+    $f3 = Base::instance();
+
+    //Define a default route
+    $f3->route('GET /', function() {
+        $view = new View;
+        echo $view->render('home.html');
+    });
+
+    //Run Fat-Free
+    $f3->run();
+
+?>
